c766f62732fae6d52f1c3c4fc197e121dc88c827
Richard Foltyn <richard.foltyn@gmail.com>
2016-11-17 00:16:12 +0100

    common: Add casts for built-in types

--- src/corelib/common/casts.F90
+++ src/corelib/common/casts.F90
@@ -7,14 +7,18 @@ module corelib_common_casts
     private
 
     interface dynamic_cast
-        module procedure cast_any_to_int32
+        module procedure cast_any_to_int32, cast_any_to_array_int32, &
+            cast_any_to_int64, cast_any_to_array_int64, &
+            cast_any_to_real32, cast_any_to_array_real32, &
+            cast_any_to_real64, cast_any_to_array_real64, &
+            cast_any_to_logical, cast_any_to_array_logical
     end interface
 
     public :: dynamic_cast
 
 contains
 
-subroutine cast_any_to_int32(tgt, ptr, status)
+subroutine cast_any_to_int32 (tgt, ptr, status)
     integer, parameter :: INTSIZE = int32
     class (*), intent(in), target :: tgt
     integer (INTSIZE), intent(out), pointer :: ptr
@@ -32,4 +36,176 @@ subroutine cast_any_to_int32(tgt, ptr, status)
 
     if (present(status)) status = lstatus
 end subroutine
+
+subroutine cast_any_to_array_int32 (tgt, ptr, status)
+    integer, parameter :: INTSIZE = int32
+    class (*), intent(in), dimension(:), target :: tgt
+    integer (INTSIZE), intent(out), dimension(:), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (integer(INTSIZE))
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+subroutine cast_any_to_int64 (tgt, ptr, status)
+    integer, parameter :: INTSIZE = int64
+    class (*), intent(in), target :: tgt
+    integer (INTSIZE), intent(out), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (integer(INTSIZE))
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+subroutine cast_any_to_array_int64 (tgt, ptr, status)
+    integer, parameter :: INTSIZE = int64
+    class (*), intent(in), dimension(:), target :: tgt
+    integer (INTSIZE), intent(out), dimension(:), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (integer(INTSIZE))
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+subroutine cast_any_to_real32 (tgt, ptr, status)
+    integer, parameter :: PREC = real32
+    class (*), intent(in), target :: tgt
+    real (PREC), intent(out), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (real(PREC))
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+subroutine cast_any_to_array_real32 (tgt, ptr, status)
+    integer, parameter :: PREC = real32
+    class (*), intent(in), dimension(:), target :: tgt
+    real (PREC), intent(out), dimension(:), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (real(PREC))
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+subroutine cast_any_to_real64 (tgt, ptr, status)
+    integer, parameter :: PREC = real64
+    class (*), intent(in), target :: tgt
+    real (PREC), intent(out), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (real(PREC))
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+subroutine cast_any_to_array_real64 (tgt, ptr, status)
+    integer, parameter :: PREC = real64
+    class (*), intent(in), dimension(:), target :: tgt
+    real (PREC), intent(out), dimension(:), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (real(PREC))
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+
+subroutine cast_any_to_logical (tgt, ptr, status)
+    class (*), intent(in), target :: tgt
+    logical, intent(out), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (logical)
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+subroutine cast_any_to_array_logical (tgt, ptr, status)
+    class (*), intent(in), dimension(:), target :: tgt
+    logical, intent(out), dimension(:), pointer :: ptr
+    integer, intent(out), optional :: status
+
+    integer :: lstatus
+
+    lstatus = STATUS_UNSUPPORTED_OPERATION
+
+    select type (tgt)
+    type is (logical)
+        ptr => tgt
+        lstatus = STATUS_OK
+    end select
+
+    if (present(status)) status = lstatus
+end subroutine
+
+
 end module
