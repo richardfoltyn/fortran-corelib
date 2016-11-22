0237400b9467ec746e997698d4a418728c0da6ec
Richard Foltyn <richard.foltyn@gmail.com>
2016-11-22 19:58:17 +0100

    argparse: Start work on unit tests

---
+++ src/tests/argparse/test_argparse.f90
@@ -0,0 +1,88 @@
+program test_strings
+
+    use iso_fortran_env
+    use corelib_argparse
+    use corelib_testing
+    use corelib_strings
+
+    implicit none
+
+    call test_all ()
+
+contains
+
+subroutine test_all()
+
+    type (test_suite) :: tests
+
+    call tests%set_label ("corelib_argparse unit tests")
+
+    ! run individual test cases
+    call test_append (tests)
+
+    ! print test statistics
+    call tests%print ()
+
+end subroutine
+
+subroutine test_append (tests)
+    class (test_suite) :: tests
+    class (test_case), pointer :: tc
+
+    type (argparser) :: parser
+    integer :: status
+
+    type (str) :: name, val
+    ! emulated command line arguments
+    type (str), dimension(:), allocatable :: cmd, val_list
+    integer :: nargs
+    logical :: is_present
+
+
+    tc => tests%add_test("argparse ACTION_APPEND")
+
+    call parser%init ("argparse append test")
+    call parser%add_argument ("name", "n", action=ARGPARSE_ACTION_APPEND, status=status)
+
+    allocate (cmd(1))
+    cmd(1) = "--name=foo"
+
+    call parser%parse (cmd, status)
+    deallocate (cmd)
+
+    call parser%is_present ("name", is_present)
+    call tc%assert_true (is_present, &
+        "ACTION_APPEND: argument present, nargs=1")
+
+    call parser%get ("name", val)
+    call tc%assert_true (val == "foo", &
+        "ACTION_APPEND: correct argument value, nargs=1")
+
+    call parser%get_nargs ("name", nargs)
+    call tc%assert_true (nargs == 1, &
+        "ACTION_APPEND: corrent # of arguments reported, nargs=1")
+
+    ! test with multiple arguments
+    allocate(cmd(1))
+    cmd(1) = "--name=foo,bar"
+
+    call parser%parse (cmd, status)
+    deallocate (cmd)
+
+    call parser%is_present ("name", is_present)
+    call tc%assert_true (is_present, &
+        "ACTION_APPEND: argument present, nargs=2")
+
+    call parser%get_nargs ("name", nargs)
+    call tc%assert_true (nargs == 2, &
+        "ACTION_APPEND: corrent # of arguments reported, nargs=2")
+
+    allocate (val_list(nargs))
+    call parser%get ("name", val_list)
+    call tc%assert_true (val_list(1) == "foo" .and. val_list(2) == "bar", &
+        "ACTION_APPEND: correct argument value, nargs=2")
+
+
+end subroutine
+
+end program
