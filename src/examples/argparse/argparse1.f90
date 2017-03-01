2b781a3e515a9a674e84d4b3e6f1bb931abecfaa
Richard Foltyn <richard.foltyn@gmail.com>
2017-03-01 08:58:14 +0100

    Examples: Expand argparse example

--- src/examples/argparse/argparse1.f90
+++ src/examples/argparse/argparse1.f90
@@ -8,26 +8,37 @@ program argparse1
     type (status_t) :: status
 
     type (str) :: name, opt
-    character (20) :: opt_char
+    character (20) :: opt_char, opt_array(2)
     logical :: flag1, flag2
-    logical, dimension(2) :: flag3
     integer :: const
 
     call parser%init ("argparse example 1")
+
+    print *, "Adding argument name"
     call parser%add_argument ("name", "n", required=.true., status=status)
+
+    print *, "Adding argument flag1"
     call parser%add_argument ("flag1", action=ARGPARSE_ACTION_STORE_TRUE, &
         status=status)
+
+    print *, "Adding argument flag2"
     call parser%add_argument ("flag2", action=ARGPARSE_ACTION_STORE_FALSE, &
         status=status)
-    call parser%add_argument ("flag3", action=ARGPARSE_ACTION_STORE_CONST, &
-        const=[.true.,.false.], default=[.false., .false.], status=status)
-    call parser%add_argument ("opt", "o", default="Default opt", status=status)
+
+    print *, "Adding argument const"
     call parser%add_argument ("const", "c", action=ARGPARSE_ACTION_STORE_CONST, &
         const=123, default=0, status=status)
 
-    ! test adding an argument with an existing name
-    call parser%add_argument ("name")
+    print *, "Adding argument opt_array"
+    call parser%add_argument ("opt_array", nargs=2, default=["foo", "bar"], status=status)
 
+    print *, "Adding argument opt"
+    call parser%add_argument ("opt", "o", default="Default option", status=status)
+
+    ! test adding an argument with an existing name
+    ! call parser%add_argument ("name", status=status)
+
+    print *, "Parsing command line"
     call parser%parse ()
 
     call parser%get ("name", name, status=status)
@@ -39,8 +50,9 @@ program argparse1
     call parser%get ("flag2", flag2)
     print *, "Argument 'flag2': ", flag2
 
-    call parser%get ("flag3", flag3)
-    print '(tr1, a, *(l1,:,","))', "Argument 'flag3': ",  flag3
+    opt_array = ""
+    call parser%get ("opt_array", opt_array)
+    print '(tr1, a, *(a,:,","))', "Argument 'opt_array': ",  opt_array
 
     call parser%get ("opt", opt)
     print *, "Argument 'opt': ", opt%to_char()
