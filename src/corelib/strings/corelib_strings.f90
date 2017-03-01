8140bf7686e54ea691526cad338585f5254e1c11
Richard Foltyn <richard.foltyn@gmail.com>
2017-03-01 15:33:42 +0100

    strings: Make strings module more backwards-compatible

--- src/corelib/strings/corelib_strings.f90
+++ src/corelib/strings/corelib_strings.f90
@@ -2,8 +2,9 @@ module corelib_strings
 
     ! Compatibility module: import corelib_common_base, which now
     ! defines the str type.
-    use corelib_common_constants
-    use corelib_common_base
-    implicit none
+    use corelib_common_base, only: str, str_array, len, len_trim, repeat, &
+        index, trim, is_alnum, is_alpha, is_digit, dynamic_cast, &
+        operator(==), operator(/=), operator(//), assignment (=)
 
+    implicit none
 end module
