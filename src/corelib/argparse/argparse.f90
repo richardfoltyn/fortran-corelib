28704dd2bcd28ea998aa938d66cda09f60512a4d
Richard Foltyn <richard.foltyn@gmail.com>
2017-03-15 22:02:15 +0100

    Build: Fix build issue due to obsolete mod files

--- src/corelib/argparse/argparse.f90
+++ src/corelib/argparse/argparse.f90
@@ -2,7 +2,7 @@ module corelib_argparse
 
     use corelib_common_constants
     use corelib_common_base
-    use corelib_argparse_constants
+    use corelib_argparse_actions
     use corelib_argparse_parser
     use corelib_argparse_validators
 
