0432a3001bb0df5ead63fb05586aaadfa4d3f6e8
Richard Foltyn <richard.foltyn@gmail.com>
2017-12-02 17:42:40 +0100

    common: add STRINGS module that exports only string-relevant stuff

---
+++ src/fcore/common/strings.f90
@@ -0,0 +1,25 @@
+
+
+module fcore_strings
+    !*  Module exports string-related types and routines from FCORE_COMMON_BASE.
+    !   Can be USEd by client code to import only interfaces relevant
+    !   to string manipulation and omit other stuff in FCORE_COMMON_BASE.
+
+    use fcore_common_base, only: &
+        str, &
+        str_array, &
+        char, &
+        len, &
+        len_trim, &
+        repeat, &
+        index, &
+        trim, &
+        is_alnum, &
+        is_alpha, &
+        is_digit, &
+        dynamic_cast, &
+        assignment(=), &
+        operator (==), &
+        operator (//)
+
+end module
