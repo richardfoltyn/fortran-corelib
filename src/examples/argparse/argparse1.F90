3c3f42e92bccd41b51cdc91e62a3b7c6119de040
Richard Foltyn <richard.foltyn@gmail.com>
2018-03-30 18:29:50 +0200

    examples: Update argparse examples

--- src/examples/argparse/argparse1.F90
+++ src/examples/argparse/argparse1.F90
@@ -12,13 +12,10 @@ program argparse1
     integer :: const
     character (10) :: opt_char
 
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
     integer :: i, n
-    type (str), dimension(:), allocatable :: str_arr
-    integer, dimension(:), allocatable :: int_arr
-    type (str), dimension(2) :: str_arr_default
-    character (10) :: opt_array(2)
-#endif
+    type (str), dimension(:), allocatable :: swork
+    character (:), dimension(:), allocatable :: cwork
+    integer, dimension(:), allocatable :: iwork
 
     call parser%init ("argparse example 1")
 
@@ -41,35 +38,39 @@ program argparse1
         const=123, default=0, status=status)
     call status%print ()
 
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
     print *, "Adding argument opt-char-arr"
-    call parser%add_argument ("opt-char-arr", nargs=2, default=["foo", "baz"], &
-        status=status)
-    call status%print ()
-#endif
 
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
-    print *, "Adding argument opt-int-arr"
-    call parser%add_argument ("opt-int-arr", nargs=2, default=[123, 456], &
+    allocate (character (3) :: cwork(2))
+    cwork(1) = "foo"
+    cwork(2) = "baz"
+    call parser%add_argument ("opt-char-arr", nargs=2, default=cwork, &
         status=status)
     call status%print ()
-#endif
+    deallocate (cwork)
+
+    print *, "Adding argument opt-int-arr"
+    allocate (iwork(2))
+    iwork(1) = 123
+    iwork(2) = 456
+    call parser%add_argument ("opt-int-arr", nargs=2, default=iwork, &
+        status=status)
+    call status%print ()
+    deallocate (iwork)
 
     print *, "Adding argument opt-str"
     call parser%add_argument ("opt-str", nargs=1, default=str("opt-str default"), &
         status=status)
     call status%print ()
 
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
     ! Note: using temporary str(:) array for default values crashes with gfortran
     ! and returns junk default values with ifort
     print *, "Adding argument opt-str-arr"
-    str_arr_default(1) = "foo"
-    str_arr_default(2) = "bar baz"
-    call parser%add_argument ("opt-str-arr", nargs=2, &
-        default=str_arr_default, status=status)
+    allocate (swork(2))
+    swork(1) = "foo"
+    swork(2) = "bar baz"
+    call parser%add_argument ("opt-str-arr", nargs=2, default=swork, status=status)
     call status%print ()
-#endif
+    deallocate (swork)
 
     print *, "Adding argument opt"
     call parser%add_argument ("opt", "o", default="Default opt", status=status)
@@ -98,55 +99,47 @@ program argparse1
     print *, "Argument 'flag2': ", flag2
     call status%print ()
 
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
-    opt_array = ""
-    call parser%get ("opt-char-arr", opt_array, status)
-    print '(tr1, a, *(a,:,", "))', "Argument 'opt-char-arr': ",  opt_array
+    allocate (character (3) :: cwork(2))
+    call parser%get ("opt-char-arr", cwork, status)
+    print '(tr1, a, *(a,:,", "))', "Argument 'opt-char-arr': ",  cwork
     call status%print ()
-#endif
+    deallocate (cwork)
 
-
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
     ! Test retrieving as str array. This makes more sense as str array
     ! does not impose requiredment that all elements are the same length.
-    allocate (str_arr(2))
-    call parser%get ("opt-char-arr", str_arr, status)
+    allocate (swork(2))
+    call parser%get ("opt-char-arr", swork, status)
     call status%print ()
     if (status == FC_STATUS_OK) then
         print *, "Argument 'opt-char-arr', retrieved as type(str) array"
-        do i = 1, size(str_arr)
-            print '(t10, "(", i0, ") ", a)', i, str_arr(i)%to_char()
+        do i = 1, size(swork)
+            print '(t10, "(", i0, ") ", a)', i, swork(i)%to_char()
         end do
     end if
-    deallocate (str_arr)
-#endif
+    deallocate (swork)
 
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
     ! Test retrieving integer array value
     print *, "== Argument 'opt-int-arr' =="
     n = parser%get_nvals ("opt-int-arr")
-    allocate (int_arr(n))
-    call parser%get ("opt-int-arr", int_arr, status)
+    allocate (iwork(n))
+    call parser%get ("opt-int-arr", iwork, status)
     if (status == FC_STATUS_OK) then
-        print '(tr1, a, *(i0, :, ", "))', "-- Values : ", int_arr
+        print '(tr1, a, *(i0, :, ", "))', "-- Values : ", iwork
     end if
     call status%print ()
-    deallocate (int_arr)
-#endif
+    deallocate (iwork)
 
-#ifndef __FCORE_GFORTRAN_POLY_ARRAY_BUG
     ! Retrieve opt-str-arr
-    allocate (str_arr(2))
-    call parser%get ("opt-str-arr", str_arr, status)
+    allocate (swork(2))
+    call parser%get ("opt-str-arr", swork, status)
     call print_result_header (parser, 'opt-str-arr', status)
     if (status == FC_STATUS_OK) then
         print *, "-- Values:"
-        do i = 1, size(str_arr)
-            print '(t5, "(", i0, ") ", a)', i, str_arr(i)%to_char()
+        do i = 1, size(swork)
+            print '(t5, "(", i0, ") ", a)', i, swork(i)%to_char()
         end do
     end if
-    deallocate (str_arr)
-#endif
+    deallocate (swork)
 
     call parser%get ("opt", opt, status)
     print *, "Argument 'opt': ", opt%to_char()
